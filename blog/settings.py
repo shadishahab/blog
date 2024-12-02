3bbaeed351deded53e0443ad0ef66a79220a19f0
shadi <shadi.shahabrazavi@gmail.com>
2024-12-02 16:29:26 +0330

    add rest framework

--- blog/settings.py
+++ blog/settings.py
@@ -127,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ) if DEBUG else (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}

f0ad122959d846aeb8b95fe34692da37b9a980cf
shadi <shadi.shahabrazavi@gmail.com>
2024-12-03 20:29:27 +0330

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    written_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='posts',
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='posts',
+        on_delete = models.DO_NOTHING,
+        blank=True
+        )
+    written_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='posts',
+        on_delete=models.CASCADE
+        )
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post,
+        related_name='comments',
+        on_delete=models.CASCADE
+        )
+    written_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='posts',
+        on_delete=models.CASCADE
+        )
+    def __str__(self):
+        return f"Comment by {self.written_by.username} on {self.post.title}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -3,6 +3,30 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    ADMIN = 'admin'
+    AUTHOR = 'author'
+    READER = 'reader'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (AUTHOR, 'Author'),
+        (READER, 'Reader'),
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    def is_author(self):
+        return self.role == self.AUTHOR
+
+    def is_reader(self):
+        return self.role == self.READER
     
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
     def __str__(self):
         return self.username

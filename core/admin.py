32c80a0568f2c843b8ea6dd7a505cbb103e661d0
shadi <shadi.shahabrazavi@gmail.com>
2024-12-03 20:36:52 +0330

    registered models in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+admin.site.register(Tag, TagAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ['id', 'username','first_name', 'last_name', 'role', 'date_joined']
+    list_display_links = ['id', 'username']
+
+admin.site.register(CustomUser, CustomUserAdmin)

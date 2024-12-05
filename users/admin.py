d36cdc41985d423a868f0d0bcaddd2b2d63a9d2d
shadi <shadi.shahabrazavi@gmail.com>
2024-12-05 15:16:23 +0330

    add 'role' field to CustomUser admin panel

--- users/admin.py
+++ users/admin.py
@@ -6,4 +6,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['id', 'username','first_name', 'last_name', 'role', 'date_joined']
     list_display_links = ['id', 'username']
 
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('role',)}),
+    )
+
 admin.site.register(CustomUser, CustomUserAdmin)
